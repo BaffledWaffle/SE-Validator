42d941d5de18af8ffd9ca2fcdc0b2011e3a5a2aa
JevGolo <jev.golosov@gmail.com>
2025-08-09 20:35:04 +0300

    Created values by default, added help info

--- src/main/java/me/BaffledWaffle/App.java
+++ src/main/java/me/BaffledWaffle/App.java
@@ -1,13 +1,37 @@
 package me.BaffledWaffle;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
- * Hello world!
- *
+ * Program main class. Parses user input
  */
-public class App 
+public class App
 {
+
+    // values by default
+    private static Path input = Paths.get( "./projects" );
+    private static Path output = Paths.get( "./report.html" );
+    private static Path vnuValidator = Paths.get( "./vnu.jar" );
+    private static Path cssValidator = Paths.get( "./css-validator.jar" );
+
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        printHelp();
+    }
+
+    private static void printHelp() {
+        System.out.println( "KASUTAMINE" );
+        System.out.println( "   java -jar SE-Validator.jar [OPTS]" );
+        System.out.println();
+        System.out.println( "OPTSIOONID" );
+        System.out.println( "   -i         [i]nput - määrab kataloogi valideeritavate projektidega (vaikimisi: " + input + ")" );
+        System.out.println( "   -o         [o]utput - määrab aruande faili nimi (vaikimisi: " + output + ")" );
+        System.out.println( "   -vnu       [vnu].jar - määrab Nu validaatori JAR faili (vaikimisi : " + vnuValidator + ")" );
+        System.out.println( "   -css       [css]-validator.jar - määrab CSS validaatori faili (vaikimisi: " + cssValidator + ")" );
+        System.out.println();
+        System.out.println( "P.S." );
+        System.out.println( "   Täpsem teave kasutamise ja valideerijate allalaadimise kohta on leitav aadressil: https://github.com/BaffledWaffle/SE-Validator"  );
     }
 }
