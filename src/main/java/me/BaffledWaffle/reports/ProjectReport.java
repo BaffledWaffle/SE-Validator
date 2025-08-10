a2ab13957519ebf94cabbd8db26cd50af9692a14
JevGolo <jev.golosov@gmail.com>
2025-08-10 11:51:29 +0300

    Add ProjectReport logic.

--- src/main/java/me/BaffledWaffle/reports/ProjectReport.java
+++ src/main/java/me/BaffledWaffle/reports/ProjectReport.java
@@ -1,4 +1,23 @@
 package me.BaffledWaffle.reports;
 
+import me.BaffledWaffle.files.FileNode;
+
 public class ProjectReport {
+
+    private String name; // project group's name
+    private FileNode fileTree;
+
+    public ProjectReport( String name, FileNode fileTree ) {
+        this.name = name;
+        this.fileTree = fileTree;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public FileNode gitFileTree() {
+        return this.fileTree;
+    }
+
 }
