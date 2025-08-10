495d9ad3439973b47550b4ba37304e453b44cda2
JevGolo <jev.golosov@gmail.com>
2025-08-10 11:58:05 +0300

    Add FileNode logic.

--- src/main/java/me/BaffledWaffle/files/FileNode.java
+++ src/main/java/me/BaffledWaffle/files/FileNode.java
@@ -1,4 +1,53 @@
 package me.BaffledWaffle.files;
 
+import me.BaffledWaffle.reports.FileReport;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileNode {
+
+    private final Path path; // file absolute path
+    private final boolean isDirectory;
+    private List<FileNode> children;
+
+    private FileReport report;
+
+    public FileNode( Path path, boolean isDirectory ) {
+        this.path = path;
+        this.isDirectory = isDirectory;
+
+        if( isDirectory )
+            children = new ArrayList<>();
+    }
+
+    public void addChild( FileNode fileNode ) {
+        children.add( fileNode );
+    }
+
+    public void setReport( FileReport report ) {
+        this.report = report;
+    }
+
+    public FileReport getReport() {
+        return report;
+    }
+
+    public List<FileNode> getChildren() {
+        return children;
+    }
+
+    public boolean isDirectory() {
+        return isDirectory;
+    }
+
+    public Path getPath() {
+        return path.toAbsolutePath().normalize();
+    }
+
+    public String getName() {
+        return path.getFileName().toString();
+    }
+
 }
